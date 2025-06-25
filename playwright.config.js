eb3d83015a1e98fa9edc8fa017bc21275b8e7ef1
marciosc83 <marciosc@gmail.com>
2025-06-25 17:13:44 -0300

    Adding actions and encapsulation

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['playwright-tesults-reporter', {'tesults-target': process.env.TOKEN }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
